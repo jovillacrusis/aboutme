c89cc39d0bed7f3360180b46786e8da0007396d3
jo <46455811+jovillacrusis@users.noreply.github.com>
2019-02-03 17:01:54 -0500

    update js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,37 @@
-// Interacting
+// Interactivity demo 2
+
+// zoom in and out of the moon
+var trigger = document.querySelector('.trigger')
+var body = document.querySelector('body')
+
+trigger.addEventListener('click', zoom)
+
+function zoom() {
+	console.log('it works')
+
+	if (body.id != 'zoom') {
+        body.id = 'zoom'
+	} else {
+		body.id = ''
+	}
+
+}
+
+
+// Hover Text
+
+var item = document.getElementById(".me");
+item.addEventListener("mouseover", func, false);
+item.addEventListener("mouseout", func1, false);
+
+function func()
+{   
+   document.getElementById("text").setAttribute("style", "display:block;")
+}
+
+function func1()
+{  
+    document.getElementById("text").setAttribute("style", "display:none;")
+}
+
+
